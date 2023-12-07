0e8f1ec0526d1856b0c8eaba74f7ec7c719f6fba
fengxie <1015806955@qq.com>
2023-12-07 11:25:35 +0800

    添加错误处理，以及 then()参数的可选

--- 12.6/codeQuestion2.js
+++ 12.6/codeQuestion2.js
@@ -20,7 +20,12 @@ class MyPromise {
 	errorFn = [];
 
 	constructor(executor) {
-		executor(this.resolve, this.reject);
+		// 捕获执行器的执行错误
+		try {
+			executor(this.resolve, this.reject);
+		} catch (error) {
+			this.reject(error);
+		}
 	}
 
 	resolve = (value) => {
@@ -44,30 +49,57 @@ class MyPromise {
 	};
 
 	then(success, error) {
+		success = success ? success : (value) => value;
+		error = error
+			? error
+			: (reason) => {
+					throw reason;
+			  };
 		// then 的回调里，如果结果是 普通值，则直接 resolve 出去
 		// 如果是 Promise 对象，则根据状态来决定调用 resolve 还是 reject
 		let thenResult = new MyPromise((resolve, reject) => {
 			if (this.status === FULFILLED) {
 				setTimeout(() => {
-					let result = success(this.value);
-					resolvePromise(thenResult, result, resolve, reject);
+					try {
+						let result = success(this.value);
+						// resolvePromise(thenResult, result, resolve, reject);
+						if (result instanceof MyPromise) {
+							result.then(resolve, reject);
+						} else {
+							resolve(result);
+						}
+					} catch (error) {
+						reject(error);
+					}
 				}, 0);
 			} else if (this.status === REJECTED) {
 				setTimeout(() => {
-					let result = error(this.reason);
-					resolvePromise(thenResult, result, resolve, reject);
+					try {
+						let result = error(this.reason);
+						resolvePromise(thenResult, result, resolve, reject);
+					} catch (error) {
+						reject(error);
+					}
 				}, 0);
 			} else {
 				this.successFn.push(() => {
 					setTimeout(() => {
-						let result = success(this.value);
-						resolvePromise(thenResult, result, resolve, reject);
+						try {
+							let result = success(this.value);
+							resolvePromise(thenResult, result, resolve, reject);
+						} catch (error) {
+							reject(error);
+						}
 					});
 				});
 				this.errorFn.push(() => {
 					setTimeout(() => {
-						let result = error(this.reason);
-						resolvePromise(thenResult, result, resolve, reject);
+						try {
+							let result = error(this.reason);
+							resolvePromise(thenResult, result, resolve, reject);
+						} catch (error) {
+							reject(error);
+						}
 					});
 				});
 			}
@@ -90,40 +122,55 @@ const resolvePromise = (thenResult, successResult, resolve, reject) => {
 };
 
 let promise1 = new MyPromise((resolve, reject) => {
-	resolve(1111);
+	// throw new Error("执行器错误");
+	// resolve(1111);
+	reject("失败");
 });
 promise1
+	.then()
+	.then()
 	.then(
 		(res) => {
-			console.log("第一次 then，接收成功的同步数据", res);
-			return new MyPromise((resolve, reject) => {
-				setTimeout(() => {
-					resolve(2222);
-				}, 1000);
-			});
+			console.log(res);
 		},
-		(err) => {
-			console.log("第一次 then，接收失败的同步数据", err);
-		},
-	)
-	.then(
-		(res2) => {
-			console.log("第二次 then，接收成功的异步数据", res2);
-			return new MyPromise((resolve, reject) => {
-				setTimeout(() => {
-					resolve(3333);
-				}, 2000);
-			});
-		},
-		(err2) => {
-			console.log("第二次 then，接收失败的同步数据", err2);
-		},
-	)
-	.then(
-		(res3) => {
-			console.log("第三次 then，接收成功的异步数据", res3);
-		},
-		(err3) => {
-			console.log("第三次 then，接收失败的异步数据", err3);
+		(error) => {
+			console.log(error);
 		},
 	);
+// promise1
+// 	.then(
+// 		(res) => {
+// 			console.log("第一次 then，接收成功的同步数据", res);
+// 			// throw new Error("then error");
+// 			return new MyPromise((resolve, reject) => {
+// 				setTimeout(() => {
+// 					resolve(2222);
+// 				}, 1000);
+// 			});
+// 		},
+// 		(err) => {
+// 			console.log("第一次 then，接收失败的同步数据", err);
+// 			return 111111;
+// 		},
+// 	)
+// 	.then(
+// 		(res2) => {
+// 			console.log("第二次 then，接收成功的异步数据", res2);
+// 			return new MyPromise((resolve, reject) => {
+// 				setTimeout(() => {
+// 					resolve(3333);
+// 				}, 2000);
+// 			});
+// 		},
+// 		(err2) => {
+// 			console.log("第二次 then，接收失败的同步数据", err2);
+// 		},
+// 	)
+// 	.then(
+// 		(res3) => {
+// 			console.log("第三次 then，接收成功的异步数据", res3);
+// 		},
+// 		(err3) => {
+// 			console.log("第三次 then，接收失败的异步数据", err3);
+// 		},
+// 	);
